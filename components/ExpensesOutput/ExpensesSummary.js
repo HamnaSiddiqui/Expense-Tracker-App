f480ab19930f9fd474220a10bf94e8e9172d1943
HamnaSiddiqui <hamnas200@gmail.com>
2023-07-06 11:39:49 +0500

    expenses component

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text></Text>
+      <Text></Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;
